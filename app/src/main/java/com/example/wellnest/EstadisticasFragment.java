384faf6fca779e4a340bfdb13d7f801b0331e572
paulariossanchez <80828204+paulariossanchez@users.noreply.github.com>
2024-12-26 00:23:17 +0100

    Base página estadísticas

--- app/src/main/java/com/example/wellnest/EstadisticasFragment.java
+++ app/src/main/java/com/example/wellnest/EstadisticasFragment.java
@@ -1,8 +1,20 @@
 package com.example.wellnest;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class EstadisticasFragment extends Fragment {
-    public static int nav_estadisticas = 1000012;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflar el layout asociado al fragmento
+        return inflater.inflate(R.layout.fragment_estadisticas, container, false);
+    }
 }
